f12af93615095ada514567cda3440b4c2c85a808
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-10-01 19:13:09 +0500

    added secure password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def index
     @users = User.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help'
   resources :friendships, only: [:create, :update, :destroy]
   devise_for :users
   get 'users/new'
   get 'users/index'
+
+  devise_scope :user do 
+    get "/users/sign_out" => "devise/sessions#destroy" 
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
