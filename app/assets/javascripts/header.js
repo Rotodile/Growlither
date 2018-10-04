85021e10292457ecc0905afd8d49454107bd3d15
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-10-04 20:28:11 +0500

    added uglifier

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,6 +1,9 @@
-function showMenu() {
-    const menu = document.getElementsByClassName("dropdown_menu")[0];
+function toggleNotification() {
+  const dropDown = document.querySelector(".dropdown");
+  dropDown.classList.toggle("dropdown-visible");
+  
+  const menu = document.getElementsByClassName("dropdown_menu")[0];
     if (menu) {
       menu.classList.toggle("visible");
     }
-  }
+}
