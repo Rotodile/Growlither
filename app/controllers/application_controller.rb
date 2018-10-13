ef33a457ecbbf5c0fde44f5caa2a7774ab580c92
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-10-13 16:26:19 +0500

    soloved error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :get_requests, if: -> { current_user }
-  
-    private
-
-  
-    def get_requests
-      @requests = current_user.requests_received.includes(:sender)
-    end
 end
